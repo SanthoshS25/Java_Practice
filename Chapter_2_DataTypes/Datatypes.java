581176882f029d6595de60c8317edfe8ea71f150
SanthoshS25 <santhoshnarayanan2004@gmail.com>
2026-02-27 20:57:11 +0530

    Chapter 2- Datatypes

---
+++ Chapter_2_DataTypes/Datatypes.java
@@ -0,0 +1,15 @@
+package Chapter_2_DataTypes;
+
+public class Datatypes {
+    public static void main(String[] args) {
+        int age = 51;
+        double price = 99.99;
+        char grade = 'A';
+        boolean isJavaFun = false;
+
+        System.out.println("Age: " + age);
+        System.out.println("Price: " + price);
+        System.out.println("Grade: " + grade);
+        System.out.println("Is Java Fun: " + isJavaFun);
+    }
+}
