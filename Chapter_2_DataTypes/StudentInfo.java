e4540e89d4d6838724e016f9389ce1a6f1f21cdb
SanthoshS25 <santhoshnarayanan2004@gmail.com>
2026-03-06 10:30:52 +0530

    Chapter02 - StudentInfo.java

---
+++ Chapter_2_DataTypes/StudentInfo.java
@@ -0,0 +1,22 @@
+package Chapter_2_DataTypes;
+
+public class StudentInfo{
+    public static void main(String[] args){
+        String name = "Santhosh";
+        int age = 21;
+        double CGPA = 8.34;
+        boolean isActive = true;
+        char CityName = 'C';
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(CGPA);
+        System.out.println(isActive);
+        System.out.println(CityName);
+
+/**
+ * This method prints student information
+ * @author Santhosh
+ */
+    }
+}
