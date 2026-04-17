cb5025c328c5ba66d559c73f907be83b81f70e96
SanthoshS25 <santhoshnarayanan2004@gmail.com>
2026-04-17 11:17:16 +0530

    Chapter 05 - String Reverse 3 methods completed

--- Chapter05_Strings/StringReverseBuilder.java
+++ Chapter05_Strings/StringReverseBuilder.java
@@ -3,10 +3,13 @@ package Chapter05_Strings;
 public class StringReverseBuilder {
     public static void main(String[] args) {
         String s  = "Santhosh";
-        StringBuilder str = new StringBuilder();
-        for(int i = s.length() - 1; i>=0; i--){
-            str.append(s.charAt(i));
-        }
-        System.out.println("The reverse String is: "+ str.toString());
+        // StringBuilder str = new StringBuilder();
+        StringBuilder str = new StringBuilder(s);
+        str.reverse();
+        System.out.println(str.toString());
+        // for(int i = s.length() - 1; i>=0; i--){
+        //     str.append(s.charAt(i));
+        // }
+        // System.out.println("The reverse String is: "+ str.toString());
     }
 }
