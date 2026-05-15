c328f8b55ca1b43fab2cc8689d369f4e28faf676
SanthoshS25 <santhoshnarayanan2004@gmail.com>
2026-05-15 13:52:01 +0530

    chapter06: constructors, this keyword, static variable

--- Chapter06_OOPS/Student.java
+++ Chapter06_OOPS/Student.java
@@ -4,11 +4,23 @@ public class Student {
     // Properties
     String name;
     int age;
-    String college;
+    static String college;
+
+    public Student(String name, int age, String college) {
+        this.name = name;
+        this.age = age;
+        this.college = college;
+    }
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+        this.college = "Govt College";
+    }
 
     // Behavior
     void study() {
-        System.out.println(name + " is studying!");
+        System.out.println(name + " is studying! in " + college);
     }
 
     void introduce() {
@@ -16,13 +28,26 @@ public class Student {
                 " and I am " + age +
                 " years old!");
     }
+
     public static void main(String[] args) {
-        Student s1 = new Student();
-        s1.name = "Santhosh"; s1.age = 22; s1.college = "KCG College";
-        Student s2 = new Student();
-        s2.name = "Ravi"; s2.age = 22; s2.college = "KCG College";
-        s1.introduce();s2.introduce();
-        s1.study(); s2.study();
+        // Student s1 = new Student();
+        // s1.name = "Santhosh"; s1.age = 22; s1.college = "KCG College";
+        // Student s2 = new Student();
+        // s2.name = "Ravi"; s2.age = 22; s2.college = "KCG College";
+        Student s1 = new Student("Santhosh", 22, "KCG College");
+        Student s2 = new Student("Ravi", 22);
+        System.out.println(s1.name + " - " + s1.college);
+        System.out.println(s2.name + " - " + s2.college);
+
+        // Now change collegeName
+        college = "Anna University";
+
+        System.out.println(s1.name + " - " + s1.college);
+        System.out.println(s2.name + " - " + s2.college);
+        // s1.introduce();
+        // s2.introduce();
+        // s1.study();
+        // s2.study();
 
     }
 }
